73f1f168fd1a836356aedd159fd504bed4919093
Prashant Bajak <bajakprashant1798@gmail.com>
2021-08-03 22:48:16 +0530

    init

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCjpnp7UHk0YtPRhfkHlZ2GUVzB6xp59s0",
+    authDomain: "clone-two-d1bcb.firebaseapp.com",
+    projectId: "clone-two-d1bcb",
+    storageBucket: "clone-two-d1bcb.appspot.com",
+    messagingSenderId: "316004281269",
+    appId: "1:316004281269:web:7bf06961febc826c6a87fc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase.firestore();
---
+++ script.js
@@ -0,0 +1,75 @@
+function getItems() {
+    db.collection("items").get().then((querySnapshot) => {
+        let items = [];
+        querySnapshot.forEach((doc) => {
+            items.push({
+                id: doc.id,
+                image: doc.data().image,
+                name: doc.data().name,
+                make: doc.data().make,
+                rating: doc.data().rating,
+                price: doc.data().price
+            })
+        });
+        // console.log(items)
+        generateItems(items);
+    });    
+}
+
+function addToCart(item) {
+    let cartItem = db.collection("cart-items").doc(item.id);
+
+    cartItem.get()
+    .then(function(doc){
+        if(doc.exists) {
+            cartItem.update({
+                quantity: doc.data().quantity + 1
+            })
+        } else {
+            cartItem.set({
+                image: item.image,
+                make: item.make,
+                name: item.name,
+                rating: item.rating,
+                price: item.price,
+                quantity: 1
+            })
+        }
+    })   
+}
+
+function generateItems(items) {
+    let itemsHTML = "";
+    items.forEach((item) => {
+        let doc = document.createElement("div");
+        doc.classList.add("main-product", "mr-5");
+        doc.innerHTML = `
+            <div class="product-image w-48 h-52 bg-white rounded-lg p-4">
+                <img class="w-full h-full object-contain" src="${item.image}" alt="">
+            </div>
+            <div class="product-name text-gray-500 font-bold mt-2 text-sm">
+                ${item.name}
+            </div>
+            <div class="product-make text-green-700 ">
+                ${item.make}
+            </div>
+            <div class="product-rating text-yello-300 font-bold my-1">
+                ⭐⭐⭐⭐⭐ ${item.rating}
+            </div>
+            <div class="product-price font-bold text-gray-700 text-lg">
+                ${numeral(item.price).format('$0,0.00')}
+            </div>
+        `
+        let addToCartEl = document.createElement("div");
+        addToCartEl.classList.add("add-to-cart", "bg-yellow-500", "h-8", "w-28", "text-white", "flex", "justify-center", "items-center", "rounded", "text-md", "cursor-pointer", "hover:bg-yellow-600");
+        addToCartEl.innerText = "Add to Cart";
+        addToCartEl.addEventListener("click", function() {
+            addToCart(item);
+        })
+        doc.appendChild(addToCartEl);
+        document.querySelector(".main-section-products").appendChild(doc);
+    })
+    
+}
+
+getItems();
